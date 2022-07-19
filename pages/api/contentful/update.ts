b4d05c8c89e13eaf3713a8285d4e4e81d4acf104
Mahit Mehta <mahit.py@gmail.com>
2022-07-19 15:43:16 -0400

    Logging

--- pages/api/contentful/update.ts
+++ pages/api/contentful/update.ts
@@ -37,6 +37,7 @@ export default async function handler(
     let pagesRevalidated:string[] = [];
 
     try {
+        console.log(req.body);
         const modelId = req.body?.sys?.contentType?.sys?.id;
 
         if ([ ContentModelID.COURSE, ContentModelID.PROMOTIONAL_LABEL ].includes(modelId)) {
