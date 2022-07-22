0e2c6d0974dd1b07bccaff6e37d7071503471e29
Mahit Mehta <mahit.py@gmail.com>
2022-07-22 00:12:30 -0400

    Style Hotfix

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ const Home : NextPage<HomeProps> = ({ notificationFlags }) => {
       <header className="flex flex-col space-y-6 justify-center items-center p-3">
         <h1 
           style={{ textShadow: "0px 0px 15px rgba(255,255,255,0.45)", whiteSpace: "nowrap"}} 
-          className="text-white text-5xl font-bold md:text-6xl mt-16 text-center">
+          className="text-white text-5xl font-bold md:text-6xl mt-12 md:mt-16 text-center">
             Master Coding&nbsp;<br className="md:hidden block"/>
             <Typist cursor={<span className={classes.blinkingCursor}>|</span>} loop={true}>
                 {
