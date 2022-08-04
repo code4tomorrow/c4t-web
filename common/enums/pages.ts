1a5c2a5d8503158572c7a64b9982fe22e2924177
Mahit Mehta <mahit.py@gmail.com>
2022-08-04 19:01:27 -0400

    Endpoint Hotfix

--- common/enums/pages.ts
+++ common/enums/pages.ts
@@ -4,5 +4,5 @@ export enum Pages {
     LEARN = "/learn",
     VOLUNTEER = "/volunteer",
     ABOUT = "/about",
-    API_JOB_PREVIEW = "/api/contentful/job/preview"
+    API_JOB_PREVIEW = "/api/contentful/jobs/preview"
 }
